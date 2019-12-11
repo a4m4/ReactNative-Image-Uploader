3325a26592e828a93f3e43d35187d1093e249899
a4m4 <salimehdi144@gmail.com>
2019-12-11 19:47:24 +0500

    All Done

--- App.js
+++ App.js
@@ -66,7 +66,7 @@ export default class App extends Component {
   };
 
   render() {
-    console.log(this.state.imageSource);
+   
     return (
       <View style={{alignItems: 'center'}}>
         <ImageBackground
